810857ea7910cc724c47ee109569a74294311565
Caio Lima <ticaiolima@gmail.com>
2025-09-08 13:01:28 -0300

    feat(wip): working on 'import defer *' parsing

--- src/execution/isolate.cc
+++ src/execution/isolate.cc
@@ -6720,6 +6720,8 @@ MaybeDirectHandle<JSPromise> Isolate::RunHostImportModuleDynamicallyCallback(
         return NewRejectedPromise(this, api_context, exception);
       }
       break;
+    case ModuleImportPhase::kDefer:
+      UNREACHABLE();
   }
 
   DirectHandle<String> specifier_str;
