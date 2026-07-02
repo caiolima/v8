a4ef2921f7c47fe1053dd2ce1441492038377448
Caio Lima <caiolima@igalia.com>
2026-07-02 16:12:49 -0300

    Using bundled version of the file

--- test/mjsunit/harmony/modules-skip-import-defer-empty.mjs
+++
@@ -1,7 +0,0 @@
-// Copyright 2026 the V8 project authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Intentionally empty: the regression test that imports this module overflows
-// the stack at the InnerModuleEvaluation entry STACK_CHECK, before this
-// module's body would ever run.
